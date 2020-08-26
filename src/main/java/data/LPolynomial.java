5948ef39242ec14e2bb3e916f54098e2fa155ff4
Kevin Chen <kchen@palantir.com>
2020-08-25 21:08:32 -0700

    later problems, update LPolynomial

--- src/main/java/data/LPolynomial.java
+++ src/main/java/data/LPolynomial.java
@@ -4,18 +4,32 @@ package data;
 import java.util.Arrays;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(callSuper = false)
 public class LPolynomial {
+
+    public static final LPolynomial ZERO = new LPolynomial();
+
     public final long[] coefficients;
+
+    public LPolynomial(long... coefficients) {
+        int newLength = coefficients.length;
+        while (newLength > 0 && coefficients[newLength - 1] == 0)
+            newLength--;
+        if (coefficients.length != newLength)
+            coefficients = Arrays.copyOf(coefficients, newLength);
+        this.coefficients = coefficients;
+    }
 
     public long evaluate(long n, long mod) {
         long evaluated = 0;
         for (int i = coefficients.length; --i >= 0;)
             evaluated = (evaluated * n + coefficients[i]) % mod;
         return evaluated;
+    }
+
+    public int degree() {
+        return coefficients.length - 1;
     }
 
     public LPolynomial add(LPolynomial other, long mod) {
@@ -60,6 +74,9 @@ public class LPolynomial {
     }
 
     private LPolynomial multiplyHelper(LPolynomial other, long mod) {
+        if (other.equals(ZERO))
+            return ZERO;
+
         long[] newCoefficients = new long[coefficients.length + other.coefficients.length - 1];
         for (int i = 0; i < coefficients.length; i++)
             for (int j = 0; j < other.coefficients.length; j++) {
@@ -69,14 +86,40 @@ public class LPolynomial {
         return new LPolynomial(newCoefficients);
     }
 
-    private LPolynomial shiftUp(int shift) {
+    public LPolynomial[] divideAndRemainder(LPolynomial other, long mod) {
+        if (degree() < other.degree())
+            return new LPolynomial[] { ZERO, this };
+
+        long[] dividend = Arrays.copyOf(coefficients, coefficients.length);
+        int m = other.coefficients.length;
+        long[] newCoefficients = new long[coefficients.length - m + 1];
+        for (int i = dividend.length - m; i >= 0; i--) {
+            long q = dividend[i + m - 1] / other.coefficients[m - 1];
+            if (q == 0)
+                continue;
+            newCoefficients[i] = q;
+            for (int j = 0; j < m; j++) {
+                dividend[j + i] -= other.coefficients[j] * q;
+                dividend[j + i] %= mod;
+            }
+        }
+        return new LPolynomial[] { new LPolynomial(newCoefficients), new LPolynomial(dividend) };
+    }
+
+    public LPolynomial gcd(LPolynomial other, long mod) {
+        if (other.equals(LPolynomial.ZERO))
+            return this;
+        return other.gcd(divideAndRemainder(other, mod)[1], mod);
+    }
+
+    public LPolynomial shiftUp(int shift) {
         long[] newCoefficients = new long[coefficients.length + shift];
         for (int i = 0; i < coefficients.length; i++)
             newCoefficients[i + shift] = coefficients[i];
         return new LPolynomial(newCoefficients);
     }
 
-    private LPolynomial shiftDown(int shift) {
+    public LPolynomial shiftDown(int shift) {
         long[] newCoefficients = new long[Math.max(0, coefficients.length - shift)];
         for (int i = 0; i < newCoefficients.length; i++)
             newCoefficients[i] = coefficients[i + shift];
